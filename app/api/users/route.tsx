a42c6af11aa3c34611e53c5c6d1e6aa2677edb16
Himanshu Gupta <hgguptahimanshu29@gmail.com>
2026-06-14 17:26:23 +0530

    Initial Commit

---
+++ app/api/users/route.tsx
@@ -0,0 +1,30 @@
+import { NextRequest,NextResponse } from "next/server";
+import { currentUser } from "@clerk/nextjs/server";
+import { db,users } from "@/db";
+import { eq } from "drizzle-orm";
+
+export async function POST(req:NextRequest){
+    const user = await currentUser();
+
+    try{
+        const userResult = await db.select().from(users).where(
+            eq(users.email,user?.primaryEmailAddress?.emailAddress??'')
+        );
+
+        if(userResult.length == 0){
+            const newUser = await db.insert(users).values({
+                email:user?.primaryEmailAddress?.emailAddress??'',
+                name:user?.fullName??'New User'
+            }).returning();
+
+            return NextResponse.json({user:newUser[0]});
+        }
+        else{
+            return NextResponse.json({user:userResult[0]});
+        }
+    }
+    catch(e){
+        console.log('Error Creating User ',e);
+        return NextResponse.json({error:"Failed to create user"});
+    }
+}
