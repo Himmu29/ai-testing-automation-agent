0e6b6663a14c93bddc25e28165f6d858384d5386
Himanshu Gupta <hgguptahimanshu29@gmail.com>
2026-06-20 17:08:14 +0530

    Fetch users github repo list

--- db/schema.ts
+++ db/schema.ts
@@ -8,6 +8,18 @@ export const users = pgTable("users", {
   credits: integer("credits").default(1000).notNull(),
 });
 
+export const repositories = pgTable("repositories",{
+  id:serial("id").primaryKey(),
+  userId:integer("user_id").references(()=> users.id).notNull(),
+  repoId:integer("repo_id").notNull(),
+  name:text("name").notNull(),
+  fullName:text("full_name").notNull(),
+  private:integer("private").notNull(),
+  htmlUrl:text("html_url").notNull(),
+  description:text("description"),
+  owner:text("owner").notNull(),
+})
+
 export const posts = pgTable("posts", {
   id: serial("id").primaryKey(),
   title: text("title").notNull(),
